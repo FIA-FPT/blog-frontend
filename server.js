79809f26cadd85f1e1a0e83c76f7ff4c8d4bd65c
baolongv3 <baolongv4@gmail.com>
2021-08-03 13:39:20 +0700

    Fixed Port

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 
-
+const PORT = process.env.PORT || 8080;
 app.use(express.static('./dist/frontend/'))
 app.get('/*', (req,res) => {
   res.sendFile('index.html')
 })
 
-app.listen(8080, () => {
+app.listen(PORT, () => {
   console.log('App is running');
 })
